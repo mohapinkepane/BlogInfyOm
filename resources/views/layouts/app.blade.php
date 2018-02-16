046021d345c74ef05ee767a272f88b13d0aa55c2
Mohapi Nkepane <mohapi765@gmail.com>
2018-02-15 17:22:18 -0800

    :added image url storage in database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,9 @@
                             <!-- Menu Footer-->
                             <li class="user-footer">
                                 <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                    
+                             <a href="#" class="btn btn-default btn-flat">Profile</a>
+                               {{--  <a href="{!! route('profiles.addpicture') !!} " class="btn btn-default btn-flat">Profile</a>  --}} 
                                 </div>
                                 <div class="pull-right">
                                     <a href="{!! url('/logout') !!}" class="btn btn-default btn-flat"
